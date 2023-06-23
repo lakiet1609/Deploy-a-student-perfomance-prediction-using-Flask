4cba8394b5bb752ca1ae16d3b024f37fa7ee9182
Kiet La <kiettuanminhla1609@gmail.com>
2023-06-23 17:34:11 +0700

    data

--- src/logger.py
+++ src/logger.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(),'logs')
+logs_path = os.path.join(os.getcwd(),'logs',LOG_FILE)
 os.makedirs(logs_path,exist_ok=True)
 
 LOG_FILE_PATH= os.path.join(logs_path, LOG_FILE)
@@ -13,4 +13,5 @@ logging.basicConfig(
     format= '[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
+
 
